f7761f511c3cd0352a9c8eae214db914db8d216f
Koby Berkovich <yaakov.berkovich@here.com>
2018-07-31 10:51:38 +0300

    do not report weekends

--- timewatch.js
+++ timewatch.js
@@ -1,5 +1,8 @@
 var rows = document.querySelectorAll(".tr")
 rows.forEach(function(row){
-	//row.click();
-	//TODO: skip Friday and Saturday
+	var day = row.querySelectorAll("td")[1].querySelector("font").textContent;
+
+	if (day.indexOf(" ")>0)//TODO: skip Holidays
+		row.click();
+	
 });
